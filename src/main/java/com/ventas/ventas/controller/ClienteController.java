b7fb90a60d3105d2334f5923ebea579f52e711f7
Luis Fraga <luis5944@gmail.com>
2020-12-16 11:30:10 +0100

    Añadir ClienteController

--- src/main/java/com/ventas/ventas/controller/ClienteController.java
+++ src/main/java/com/ventas/ventas/controller/ClienteController.java
@@ -3,10 +3,9 @@ package com.ventas.ventas.controller;
 import com.ventas.ventas.model.Cliente;
 import com.ventas.ventas.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +16,42 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<List<Cliente>> findAll() {
         return ResponseEntity.ok(clienteService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    //Unimos la id del mapping con la id que le mandamos
+    public ResponseEntity<Cliente> findById(@PathVariable("id") Integer idCliente) {
+
+        return clienteService.findById(idCliente)
+                .map(c -> ResponseEntity.ok(c))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    //Creamos un cliente y que vendrá en el body
+    @PostMapping
+    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+        return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Cliente> update(@RequestBody Cliente cliente) {
+        return clienteService.findById(cliente.getIdCliente())
+                .map(c -> ResponseEntity.ok(clienteService.update(cliente)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> delete(@PathVariable("id") Integer idCliente) {
+
+        return clienteService.findById(idCliente)
+                .map(c -> {
+                    clienteService.delete(idCliente);
+                    return ResponseEntity.ok(c);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
